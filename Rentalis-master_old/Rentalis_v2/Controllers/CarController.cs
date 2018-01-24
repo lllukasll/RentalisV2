fdcb4a0deb6d53dd604ab548c0479833ce5305d1
lllukasll <L.Biedr@gmail.com>
2018-01-24 21:17:54 +0100

    Poprawki w Szczegółach wypożyczenia

--- Rentalis-master_old/Rentalis_v2/Controllers/CarController.cs
+++ Rentalis-master_old/Rentalis_v2/Controllers/CarController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mime;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.Ajax.Utilities;
 using Rentalis_v2.Models;
 using MySql.Data.MySqlClient;
 
@@ -109,15 +111,33 @@ namespace Rentalis_v2.Controllers
             carRentViewModel.days = Math.Ceiling((DateTo - DateFrom).TotalDays);
             carRentViewModel.TotalPrice = car.PricePerDay * carRentViewModel.days;
 
+            carRentViewModel.PaymentMethod = new List<SelectListItem>();
+
+            carRentViewModel.PaymentMethod.Add(new SelectListItem{Text = "Opłata przy odbiorze", Value = "1"});
+            carRentViewModel.PaymentMethod.Add(new SelectListItem{Text = "Płatność online", Value = "2"});
+
             return View(carRentViewModel);
         }
 
-        public ActionResult ConfirmRent(int id, DateTime DateFrom, DateTime DateTo, double TotalPrice, string userId)
+        public ActionResult ConfirmRent(/*int id, DateTime DateFrom, DateTime DateTo, double TotalPrice, string userId, int paymentMethod*/CarRentViewModels rentModel)
         {
-            string dT = DateTo.ToString("yyyyMMddHHmmss");
-            string dF = DateFrom.ToString("yyyyMMddHHmmss");
+            //string dT = rentModel.dateTo.ToString("yyyyddMMHHmmss");
+            //string dF = rentModel.dateFrom.ToString("yyyyddMMHHmmss");
 
-            string query = String.Format(@"INSERT INTO rentalisv2.bookingmodels VALUES (null,'{0}','{1}','{2}',{3},{4},1)", id,userId,dF,dT,TotalPrice);
+            string dT = rentModel.YearTo + rentModel.MonthTo.PadLeft(2,'0') + rentModel.DayTo.PadLeft(2, '0') + rentModel.HourTo.PadLeft(2, '0') + rentModel.MinuteTo.PadLeft(2, '0') +
+                        rentModel.SecundTo.PadLeft(2, '0');
+            string dF = rentModel.YearFrom + rentModel.MonthFrom.PadLeft(2, '0') + rentModel.DayFrom.PadLeft(2, '0') + rentModel.HourFrom.PadLeft(2, '0') + rentModel.MinuteFrom.PadLeft(2, '0') +
+                        rentModel.SecundFrom.PadLeft(2, '0');
+
+            if (rentModel.ChoosenPaymentMethod == 1)
+            {
+                
+            }else if (rentModel.ChoosenPaymentMethod == 2)
+            {
+                
+            }
+
+            string query = String.Format(@"INSERT INTO rentalisv2.bookingmodels VALUES (null,'{0}','{1}','{2}',{3},{4},1)", rentModel.car.Id, rentModel.userId, dF,dT, rentModel.TotalPrice);
             //INSERT INTO rentalisv2.bookingmodels VALUES (null,'20171010080000','20171020080000',300,'asdasdas-eeq123-dxczcc.ad',2);
             MySqlConnection conn = new MySqlConnection("SERVER=localhost;DATABASE=rentalisv2;UID=root;PASSWORD=;");
             try
--- Rentalis-master_old/Rentalis_v2/Models/CarRentViewModels.cs
+++ Rentalis-master_old/Rentalis_v2/Models/CarRentViewModels.cs
@@ -2,16 +2,34 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace Rentalis_v2.Models
 {
     public class CarRentViewModels
     {
+        public string userId { get; set; }
         public CarModels car { get; set; }
         public DateTime dateFrom { get; set; }
         public DateTime dateTo { get; set; }
         public double days { get; set; }
         public double TotalPrice { get; set; }
         public int sum { get; set; }
+        public int ChoosenPaymentMethod { get; set; }
+        public List<SelectListItem> PaymentMethod { get; set; }
+       
+        public string YearFrom { get; set; }
+        public string MonthFrom { get; set; }
+        public string DayFrom { get; set; }
+        public string HourFrom { get; set; }
+        public string MinuteFrom { get; set; }
+        public string SecundFrom { get; set; }
+
+        public string YearTo { get; set; }
+        public string MonthTo { get; set; }
+        public string DayTo { get; set; }
+        public string HourTo { get; set; }
+        public string MinuteTo { get; set; }
+        public string SecundTo { get; set; }
     }
 }
